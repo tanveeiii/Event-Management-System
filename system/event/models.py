2f10413499a06c7bdbcdd2085cd1beadbffe92be
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-17 14:43:37 +0530

    Adding a new model

--- system/event/models.py
+++ system/event/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class CoreTeam(models.Model):
+    rollNo = models.IntegerField()
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    phoneNo = models.BigIntegerField()
+    emailId = models.EmailField()
+
