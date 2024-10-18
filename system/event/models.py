b3a8a88c579f1d6499183a534bcbc04106695efb
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-18 22:51:44 +0530

    Setting up React+Vite App

--- system/event/models.py
+++ system/event/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class CoreTeam(models.Model):
+class Team(models.Model):
     rollNo = models.IntegerField()
     name = models.CharField(max_length=50)
     position = models.CharField(max_length=50)
     phoneNo = models.BigIntegerField()
     emailId = models.EmailField()
+
+
 
