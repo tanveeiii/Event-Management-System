e8f42fb8ce0c6521b7448899f0ced6f7e1fa2bb7
MananJiwnani <mananjiwnani@gmail.com>
2024-10-24 01:06:37 +0530

    competitions

--- event-frontend/src/App.jsx
+++ event-frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Homepage from './Homepage'
+import Competitions from './Competitions'
 
 function App() {
 
   return (
     <>
-      <Homepage/>
+      <Competitions/>
     </>
   )
 }
