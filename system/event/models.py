1a2d86b245640822a73e43d641ed94d51ca3a0e4
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-23 19:41:44 +0530

    Editing models

--- system/event/models.py
+++ system/event/models.py
@@ -6,23 +6,23 @@ class Team(models.Model):
     position = models.CharField(max_length=50)
     phoneNo = models.IntegerField()
     emailId = models.EmailField()
-    password = models.CharField()
+    password = models.CharField(max_length=500)
 
 class Event(models.Model):
-    eventName = models.CharField(max_length=50, primary_key=True)
+    eventName = models.CharField(max_length=50)
     eventVenue = models.CharField(max_length=100)
     eventTime = models.DateTimeField()
-    eventId = models.AutoField()
+    eventId = models.AutoField(primary_key=True)
 
 class Attendees(models.Model):
     name = models.CharField(max_length=50)
     phoneNo = models.IntegerField()
     emailId = models.EmailField()
-    ticketId = models.CharField(primary_key=True)
+    ticketId = models.CharField(primary_key=True, max_length=255)
     accommodation = models.BooleanField()
 
 class Competitions(models.Model):
-    competitionName = models.CharField()
+    competitionName = models.CharField(max_length=50)
     prizeMoney = models.IntegerField()
     date = models.DateField()
     competitionId = models.AutoField(primary_key=True)
@@ -31,18 +31,18 @@ class Participants(models.Model):
     name = models.CharField(max_length=50)
     phoneNo = models.IntegerField()
     emailId = models.EmailField()
-    registrationId = models.CharField(primary_key=True)
+    registrationId = models.CharField(primary_key=True, max_length=255)
     accommodation = models.BooleanField()
     competitionId = models.ForeignKey(Competitions, on_delete=models.CASCADE)
 
 class Speakers(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
     image = models.ImageField()
 
 class Sponsors(models.Model):
     logo = models.ImageField()
-    name = models.CharField(primary_key=True)
+    name = models.CharField(primary_key=True, max_length=50)
     sponsorshipAmount = models.IntegerField()
     dealBy = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
     phoneNo = models.IntegerField()
