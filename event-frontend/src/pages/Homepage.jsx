9c8ed3aac5b9dfafcfa78a683057671920dd1265
KMan943 <kman05108@gmail.com>
2024-10-26 21:00:20 +0530

    ok

--- event-frontend/src/pages/Homepage.jsx
+++ event-frontend/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Countdown from '../components/Countdown'
+import FluxusTitle from '../components/FluxusTitle'
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
             <Countdown />
+            <FluxusTitle/>
         </>
     )
 }
