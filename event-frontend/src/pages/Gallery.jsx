a9d0ccc47399ab419207b799607b4550eed7efee
MananJiwnani <mananjiwnani@gmail.com>
2024-10-30 17:27:54 +0530

    gallery

---
+++ event-frontend/src/pages/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HoneycombGallery from '../components/HoneycombGallery'
+import '../static/Gallery.css'
+
+
+const Gallery = () => {
+  return (
+    <>
+        <HoneycombGallery/>
+    </>
+  )
+}
+
+export default Gallery
