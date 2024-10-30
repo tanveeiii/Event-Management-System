52f6b76c5dd709dcc9cbebb93081d0c0ebf0b895
MananJiwnani <mananjiwnani@gmail.com>
2024-10-30 14:43:11 +0530

    compswiper

--- event-frontend/src/pages/Competitions.jsx
+++ event-frontend/src/pages/Competitions.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import EmblaCarousel from '../components/EmblaCarousel';
 import '../static/Competitions.css'
+import CompSwiper from '../components/CompSwiper';
+
+import FadeLoader from 'react-spinners/FadeLoader'
 
 const Competitions = () => {
-  const compListURL = 'http://localhost:8000/api/competitions/';
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+  const compListURL = 'https://run.mocky.io/v3/2a6fbd11-ed93-4cb8-b8d8-883b5a041d74';
   const [compData, setCompData] = useState([]);
 
   useEffect(() => {
@@ -14,7 +18,7 @@ const Competitions = () => {
     try {
       const response = await fetch(compListURL);
       const data = await response.json();
-      setCompData(data);
+      setCompData(data['competitions']);
       console.log(data); // Log the fetched data
     } catch (error) {
       console.error('Error fetching competitions:', error);
@@ -23,13 +27,15 @@ const Competitions = () => {
 
   return (
     <>
-      <h1 className="competitions-title">Competitions</h1>
-      {/* {compData.length > 0 ? (
-        <EmblaCarousel slides={compData} /> 
+      {/* <h1 className="competitions-title">Competitions</h1> */}
+      {compData.length > 0 ? (
+        <CompSwiper competitions={compData} />
       ) : (
-        <p>Loading competitions...</p>
-      )} */}
-      <EmblaCarousel slides={compData} /> 
+        <div className="loading">
+          <FadeLoader color='#f76c6c' radius={6} height={20} width={5}/>
+          <p>Loading Competitions...</p>
+        </div>
+      )}
     </>
   );
 }
