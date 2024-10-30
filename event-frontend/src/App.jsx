68db9a637e76e5f1e1785f4e205299766d7f3526
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-30 17:47:11 +0530

    Redirecting to dashboard after login

--- event-frontend/src/App.jsx
+++ event-frontend/src/App.jsx
@@ -6,13 +6,14 @@ import PastArtists from './pages/PastArtists'
 import Team from './pages/Team'
 import Speakers from './pages/Speakers'
 import Login from './pages/Login'
-import { RouterProvider , createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, useLocation } from 'react-router-dom'
 import MainLayout from './pages/MainLayout'
+// import { useLocation } from 'react-router-dom'
 
 const App = () => {
   const router = createBrowserRouter([
-      {
-        path: "/", element: <MainLayout />, children: [
+    {
+      path: "/", element: <MainLayout />, children: [
         {
           path: "/",
           element: <Homepage />,
@@ -35,15 +36,20 @@ const App = () => {
         },
         {
           path: "/login",
-          element: <Login />
+          element: <Login />,
+          // action: loginAction
         },
         {
           path: "/speakers",
           element: <Speakers />
         },
         {
+          path: "/dashboard",
+          element: <p style={{ "color": "white", "margin": "200px" }}>Hello</p>
+        },
+        {
           path: "/login",
-          element: <Login/>
+          element: <Login />
         },
       ]
     },
