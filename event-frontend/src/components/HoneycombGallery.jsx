5975b14e510acca944459ac22511904be9c0c5da
Tanvi Agarwal <cse230001075@iiti.ac.in>
2024-10-30 18:24:51 +0530

    Displaying gallery images from backend on frontend

--- event-frontend/src/components/HoneycombGallery.jsx
+++ event-frontend/src/components/HoneycombGallery.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import '../static/Gallery.css'
 
-const HoneycombGallery = () => {
+const HoneycombGallery = ({ gallery }) => {
+    console.log(gallery)
     return (
         <ul className="hex-grid__list">
-            <li className="hex-grid__item">
-                <div className="hex-grid__content">1</div>
-            </li>
-            <li className="hex-grid__item">
+            {
+                gallery && gallery.length > 0 ?
+                    gallery.map((images,index)=>(
+                    <li key={index} className="hex-grid__item">
+                        <div className="hex-grid__content"><img src={images.image} alt="" /></div>
+                    </li>)):<p>Loading images</p>
+            }
+            {/* <li className="hex-grid__item">
                 <div className="hex-grid__content">2</div>
             </li>
             <li className="hex-grid__item">
@@ -27,7 +32,7 @@ const HoneycombGallery = () => {
             </li>
             <li className="hex-grid__item">
                 <div className="hex-grid__content">2</div>
-            </li>
+            </li> */}
         </ul>
     )
 }
--- event-frontend/src/pages/Gallery.jsx
+++ event-frontend/src/pages/Gallery.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import HoneycombGallery from '../components/HoneycombGallery'
 import '../static/Gallery.css'
+import { useState, useEffect } from 'react'
 
 
 const Gallery = () => {
+  const [gallery, setGalleryData] = useState([])
+
+const galleryListURL = 'http://localhost:8000/api/gallery/'
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+  const fetchGallery = async () => {
+    try {
+      const response = await fetch(galleryListURL);
+      const data = await response.json();
+      setGalleryData(data)
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
   return (
     <>
-        <HoneycombGallery/>
+        <HoneycombGallery gallery={gallery}/>
     </>
   )
 }
