ed2c15e30f895ad55107567552e78c928b3dda73
MananJiwnani <mananjiwnani@gmail.com>
2024-11-09 21:02:19 +0530

    ok

--- event-frontend/src/pages/Ticket.jsx
+++ event-frontend/src/pages/Ticket.jsx
@@ -10,26 +10,26 @@ const Ticket = () => {
                 {/* <h3>Welcome</h3> */}
                 <div className="form-details">
                     <div className="T-field">
-                        <input type="text" className='T-input' placeholder='' id="name" />
+                        <input type="text" className='T-input' placeholder='' required id="name" />
                         <label htmlFor="name" className='input-label'>Name</label>
                     </div>
                     <div className="T-field">
-                        <input type="text" className='T-input' placeholder='' id="email" />
+                        <input type="text" className='T-input' placeholder='' required id="email" />
                         <label htmlFor="email" className='input-label'>Email ID</label>
                     </div>
                     <div className="T-field">
-                        <input type="text" className='T-input' placeholder='' id="contact" />
+                        <input type="text" className='T-input' placeholder='' required id="contact" />
                         <label htmlFor="contact" className='input-label'>Contact No.</label>
                     </div>
                     <div className="T-radio">
                         <span>Accommodation</span>
                         <div className="radio-group">
-                            <input type="radio" id='Acc1' name="accommodation" value="Yes" />
+                            <input type="radio" id='Acc1' required name="accommodation" value="Yes" />
                             <label className='radio-label' htmlFor='Acc1'>Yes</label>
                             <div className="radio-text">(Ticket Price: ₹2000)</div>
                         </div>
                         <div className="radio-group">
-                            <input type="radio" id='Acc0' name="accommodation" value="No" />
+                            <input type="radio" id='Acc0' required name="accommodation" value="No" />
                             <label className='radio-label' htmlFor='Acc0'>No</label>
                             <div className="radio-text">(Ticket Price: ₹500)</div>
                         </div>
