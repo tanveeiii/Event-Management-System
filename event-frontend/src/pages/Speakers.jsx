ceee5000dd8efa24e992f4359fd6c62fc405b358
KMan943 <kman05108@gmail.com>
2024-11-12 15:44:27 +0530

    restoring speakers backend link

--- event-frontend/src/pages/Speakers.jsx
+++ event-frontend/src/pages/Speakers.jsx
@@ -4,32 +4,32 @@ import "../static/Speakers.css"
 
 const Speakers = () => {
 
-  const speakers = [{name:"Mani" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"gtfdsgvbndbuk euerbgbuj srufhgb hberg hvbsjb gjdsfhb jhgbh ujsbg jdufb mdfhs bjdsfhgb jdsfhb j"} , 
-                    {name:"Manan" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
-                    {name:"Mani" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
-                    {name:"Mani" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"ilfbnamendshjgb"} , 
-                    {name:"Manan" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
-                    {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
-                    {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"ilfbnamendshjgb"} , 
-                    {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
-                    {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
-  ];
-  // const [speakers , setspeakers] = useState([]);
+  // const speakers = [{name:"Mani" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"gtfdsgvbndbuk euerbgbuj srufhgb hberg hvbsjb gjdsfhb jhgbh ujsbg jdufb mdfhs bjdsfhgb jdsfhb j"} , 
+  //                   {name:"Manan" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
+  //                   {name:"Mani" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
+  //                   {name:"Mani" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"ilfbnamendshjgb"} , 
+  //                   {name:"Manan" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
+  //                   {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
+  //                   {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-1.jpg" , title:"Entrepreneur" , description:"ilfbnamendshjgb"} , 
+  //                   {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-2.jpg" , title:"Gamer" , description:"gersagh"} , 
+  //                   {name:"Jeel" , image:"https://swiperjs.com/demos/images/nature-3.jpg" , title:"Youtube Watcher" , description:"bdhjsbgfsvz"},
+  // ];
+  const [speakers , setspeakers] = useState([]);
   
-  // useEffect( () => {
-  //   const fetchspeakers = async () => {
-  //     const apiurl = "http://localhost:8000/api/speaker/";
-  //     try {
-  //       const res = await fetch(apiurl);
-  //       const data = await res.json();
-  //       setspeakers(data);
-  //     } catch (error) {
-  //       console.log("Error fetching data");
-  //     }
-  //   }
+  useEffect( () => {
+    const fetchspeakers = async () => {
+      const apiurl = "http://localhost:8000/api/speaker/";
+      try {
+        const res = await fetch(apiurl);
+        const data = await res.json();
+        setspeakers(data);
+      } catch (error) {
+        console.log("Error fetching data");
+      }
+    }
 
-  //   fetchspeakers();
-  // } , []); 
+    fetchspeakers();
+  } , []); 
 
   return (
     <>
@@ -41,7 +41,6 @@ const Speakers = () => {
         ))
       }
       </div>
-      {/* <Speaker /> */}
     </>
   )
 }
