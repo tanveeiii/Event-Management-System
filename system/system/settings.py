9ad6981f33e4548379a0f726b5554e0a7d3093c7
MananJiwnani <mananjiwnani@gmail.com>
2024-11-13 14:33:26 +0530

    ok

--- system/system/settings.py
+++ system/system/settings.py
@@ -141,3 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOWED_ORIGINS = ["http://localhost:5173","http://127.0.0.1:5173"]
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
