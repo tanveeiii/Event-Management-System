d8f6a21b34489757b67dfeb30fffeaadb55df896
Tanvi Agarwal <agarwaltanvi1507@gmail.com>
2024-11-17 11:43:29 +0530

    Editing database tabels

--- system/event/models.py
+++ system/event/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 import uuid
 
-class Event(models.Model):
-    eventName = models.CharField(max_length=50)
-    eventVenue = models.CharField(max_length=100)
-    dayNo = models.IntegerField(choices=((1,1), (2,2), (3,3), (4,4)))
-    eventDate = models.DateField()
-    eventTime = models.TimeField()
-    eventDesc = models.CharField(max_length=250)
-    eventId = models.AutoField(primary_key=True)
 
 class Team(models.Model):
     rollNo = models.IntegerField(primary_key=True)
@@ -21,10 +13,16 @@ class Team(models.Model):
     linkedinId = models.URLField()
     teamName = models.CharField(max_length=50)
     image = models.TextField()
-    # --------------------------------------------------- #
-    eventid = models.ForeignKey(Event , on_delete=models.CASCADE)
-    # --------------------------------------------------- #
     
+class Event(models.Model):
+    eventName = models.CharField(max_length=50)
+    eventVenue = models.CharField(max_length=100)
+    dayNo = models.IntegerField(choices=((1,1), (2,2), (3,3), (4,4)))
+    eventDate = models.DateField()
+    eventTime = models.TimeField()
+    eventDesc = models.CharField(max_length=250)
+    eventId = models.AutoField(primary_key=True)
+    rollNo = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
 
 class Attendees(models.Model):
     name = models.CharField(max_length=50)
@@ -37,12 +35,7 @@ class Attendees(models.Model):
 class Competitions(models.Model):
     competitionName = models.CharField(max_length=50)
     prizeMoney = models.IntegerField()
-
-    # --------------------------------------------------- #
-    #date = models.DateField()   child of events table which already stores the date and time of the event
     eventid = models.ForeignKey(Event , on_delete=models.CASCADE)
-    # --------------------------------------------------- #
-
     poster = models.TextField()
     competitionId = models.AutoField(primary_key=True)
 
@@ -51,21 +44,13 @@ class Participants(models.Model):
     phoneNo = models.CharField(max_length=20)
     emailId = models.EmailField()
     registrationId = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    # accommodation = models.BooleanField()
-
-    # --------------------------------------------------- #
     competitionId = models.ForeignKey(Competitions, on_delete=models.CASCADE)
-    # --------------------------------------------------- #
 
 class Speakers(models.Model):
     speakerId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
     image = models.TextField()
-
-    # --------------------------------------------------- #
-    eventid = models.ForeignKey(Event , on_delete=models.CASCADE)
-    # --------------------------------------------------- #
 
 class Sponsors(models.Model):
     sponsorId = models.AutoField(primary_key=True)
@@ -77,9 +62,6 @@ class Sponsors(models.Model):
     emailId = models.EmailField()
     title = models.CharField(max_length=80)
     link = models.URLField()
-    # --------------------------------------------------- #
-    rollno = models.ForeignKey(Team , on_delete=models.CASCADE)
-    # --------------------------------------------------- #
 
 class Gallery(models.Model):
     image = models.TextField()
--- system/event/views.py
+++ system/event/views.py
@@ -8,6 +8,7 @@ import base64
 import razorpay
 from dotenv import load_dotenv
 import os
+from django.db import IntegrityError
 
 load_dotenv()
 RAZORPAY_ID = os.getenv("RAZORPAY_ID")
@@ -40,22 +41,21 @@ def competitions(request):
             competitions_list=cursor.fetchall()
         competitions_data = []
         for competition in competitions_list:
-            data_url = f"data:image/jpeg;base64,{competition[4]}"
-            competitions_data.append({"name": competition[0], "prizeMoney": competition[1], "date": competition[2], "id": competition[3], "image":data_url})
+            data_url = f"data:image/jpeg;base64,{competition[2]}"
+            competitions_data.append({"name": competition[0], "prizeMoney": competition[1], "id": competition[3], "image":data_url, "eventId": competition[4]})
         return JsonResponse(competitions_data, safe=False)
     if(request.method=="POST"):
         name = request.POST.get('name')
         prizeMoney = request.POST.get('prizeMoney')
-        date = request.POST.get("date")
         image = request.FILES.get('image')
-        print(name, prizeMoney, date, image)
+        eventId = request.POST.get('eventId')
         image_data = image.read()
         encoded_image = base64.b64encode(image_data).decode('utf-8')
         query = """
-                    insert into event_competitions(competitionName, prizeMoney, date, poster) values (%s,%s,%s,%s)
+                    insert into event_competitions(competitionName, prizeMoney, poster, eventId) values (%s,%s,%s,%s)
                 """
         with connection.cursor() as cursor:
-            cursor.execute(query, (name, prizeMoney, date, encoded_image))
+            cursor.execute(query, (name, prizeMoney, encoded_image, eventId))
         
         transaction.commit()
         return JsonResponse({'status': 'success', 'message': 'Competition added successfully.'})
@@ -63,7 +63,7 @@ def competitions(request):
     if(request.method=="DELETE"):
         data = json.loads(request.body)
         competitionId = data['id']
-        query = f"delete from event_competitions where competitionId = {competitionId}"
+        query = f"delete from event_competitions where competitionId_id = {competitionId}"
         with connection.cursor() as cursor:
             cursor.execute(query)
         transaction.commit()
@@ -84,7 +84,6 @@ def team(request):
         return JsonResponse(team_data, safe=False)
     
     if(request.method=="POST"):
-        # data = json.loads(request.body)
         name = request.POST.get('name')
         rollNo = request.POST.get('id')
         position = request.POST.get('position')
@@ -124,23 +123,36 @@ def event(request):
             event_list= cursor.fetchall()
         event_data = []
         for event in event_list:
-            event_data.append({"name": event[0], "venue": event[1], "time": event[2], "id": event[3], "desc": event[4], "dayNo":event[5], "eventDate":event[6]})
+            query = f"select name from event_team where rollNo = {event[7]}"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                managedBy = cursor.fetchone()[0]
+                event_data.append({"name": event[0], "venue": event[1], "time": event[4], "id": event[6], "desc": event[5], "dayNo":event[2], "eventDate":event[3], "managedBy": managedBy})
         return JsonResponse(event_data, safe=False)
     if(request.method=="POST"):
-        data = json.loads(request.body)
-        print(data)
-        name = data["name"]
-        venue = data["venue"]
-        time = data["time"]
-        desc = data["desc"]
-        date = data['eventDate']
-        dayNo = data['dayNo']
-        query = """
-                    insert into event_event (eventName, eventVenue, eventTime, eventDesc, dayNo, eventDate) values(%s,%s,%s,%s,%s,%s)"""
-        with connection.cursor() as cursor:
-            cursor.execute(query, (name, venue, time, desc, dayNo, date))
-        transaction.commit()
-        return JsonResponse({'status': 'success', 'message': 'Event added successfully.'})
+        try:
+            data = json.loads(request.body)
+            print(data)
+            name = data["name"]
+            venue = data["venue"]
+            time = data["time"]
+            desc = data["desc"]
+            date = data['eventDate']
+            dayNo = data['dayNo']
+            managedBy = data['managedBy']
+            query = """
+                        insert into event_event (eventName, eventVenue, eventTime, eventDesc, dayNo, eventDate, rollNo_id) values(%s,%s,%s,%s,%s,%s,%s)"""
+            query2 = f"select rollNo from event_team where name='{managedBy}'"
+            with connection.cursor() as cursor:
+                cursor.execute(query2)
+                rollNo = cursor.fetchone()[0]
+                cursor.execute(query, (name, venue, time, desc, dayNo, date,rollNo))
+            transaction.commit()
+            return JsonResponse({'status': 'success', 'message': 'Event added successfully.'})
+        except IntegrityError as e:
+            if "Cannot schedule an event in the past" in str(e):
+                return JsonResponse({"error": "Cannot schedule an event in the past."}, status=400)
+            return JsonResponse({"error": "Database error occurred."}, status=500)
     if(request.method=="DELETE"):
         data = json.loads(request.body)
         id = data['id']
@@ -160,10 +172,9 @@ def participants(request):
         phoneNo = data['phoneNo']
         emailId = data['emailId']
         competitionName = data['competitionName']
-        # accommodation = data['accommodation']
         registrationId = uuid.uuid4()
         with connection.cursor() as cursor:
-            query = f"select competitionId from event_competitions where competitionName= '{competitionName}'"
+            query = f"select competitionId_id from event_competitions where competitionName= '{competitionName}'"
             cursor.execute(query)
             competitionId = cursor.fetchone()[0]
             print(competitionId)
@@ -186,7 +197,7 @@ def participants(request):
         data = json.loads(request.body)
         id = data['id']
         with connection.cursor() as cursor:
-            query = f"delete from event_participants where eventId = {id}"
+            query = f"delete from event_participants where registrationId = {id}"
             cursor.execute(query)
         transaction.commit()
         return JsonResponse({"status":"success", "message":"Participant deleted successfully"})
@@ -200,8 +211,8 @@ def speakers(request):
             speakers_list = cursor.fetchall()
         speakers_data = []
         for speaker in speakers_list:
-            data_url = f"data:image/jpeg;base64,{speaker[2]}"
-            speakers_data.append({"name": speaker[0], "desc" : speaker[1], "image" : data_url, "id":speaker[3]})
+            data_url = f"data:image/jpeg;base64,{speaker[3]}"
+            speakers_data.append({"name": speaker[1], "desc" : speaker[2], "image" : data_url, "id":speaker[0]})
         return JsonResponse(speakers_data, safe=False)
     if(request.method=="POST"):
         name = request.POST.get('name')
@@ -266,7 +277,7 @@ def sponsors(request):
         for sponsor in sponsors_list:
             data_url = f"data:image/jpeg;base64,{sponsor[1]}"
             # print(sponsor)
-            sponsors_data.append({"name": sponsor[2], "amt" : sponsor[3], "dealBy" : sponsor[4], "phoneNo":sponsor[5], "emailId": sponsor[6], "image": data_url, "link": sponsor[8], "title":sponsor[7]})
+            sponsors_data.append({"name": sponsor[2], "amt" : sponsor[3], "dealBy" : sponsor[8], "phoneNo":sponsor[4], "emailId": sponsor[5], "image": data_url, "link": sponsor[7], "title":sponsor[6]})
         return JsonResponse(sponsors_data, safe=False)
     if(request.method=="POST"):
         name = request.POST.get('name')
