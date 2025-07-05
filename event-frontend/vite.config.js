7906d2ecd207c6a3ae812b469a4d5a94abfb8ee9
Manan <mananjiwnani@gmail.com>
2025-07-06 03:42:23 +0530

    changes

--- event-frontend/vite.config.js
+++ event-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'https://mananjiwnani.pythonanywhere.com/',
+        target:'http://127.0.0.1:8000',
         changeOrigin:true,
         rewrite:(path)=>path.replace(/^\/api/,''),
         
